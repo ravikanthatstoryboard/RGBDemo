890995a1d3c69bd260b7b60662ef353977372d9a
Ravikanth <ravikanthcse2011@gmail.com>
2018-04-18 15:32:53 +0530

    first commit

---
+++ RGBDemo.framework/Headers/RGBDemo.h
@@ -0,0 +1,19 @@
+//
+//  RGBDemo.h
+//  RGBDemo
+//
+//  Created by Ravikanth on 17/04/18.
+//  Copyright © 2018 StoryBoard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGBDemo.
+FOUNDATION_EXPORT double RGBDemoVersionNumber;
+
+//! Project version string for RGBDemo.
+FOUNDATION_EXPORT const unsigned char RGBDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGBDemo/PublicHeader.h>
+
+
